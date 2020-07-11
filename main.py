d73767460220d53999a333eb806540e59e3cd4bb
Gonzalo García <pochilandio11@gmail.com>
2020-07-11 19:20:43 +0200

    first step

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, jsonify
+import requests
+import json
+import xmltodict
+import xml
+import xml.etree.ElementTree as ET
+
+app = Flask(__name__)
+
+
+#def home():
+#    return render_template('hello.html')
+@app.route('/')
+def homepage():
+  r = requests.get(
+     'http://ergast.com/api/f1/2020/1/results.json')
+  return render_template('results_test.html', results=json.loads(r.text)['MRData']['RaceTable'])
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
